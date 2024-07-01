7f677e22c277791ede9aa913e5447a545c778900
Ravi kumar <ravikumar117211@gmail.com>
2024-07-01 07:21:32 +0530

    added env variable to react, vercel file to backend

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-//   baseURL: process.env.REACT_APP_API_BASE_URL,
-baseURL : "http://localhost:3001"
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+    // baseURL :  "http://localhost:3001"
 });
 
 export default axiosInstance;
